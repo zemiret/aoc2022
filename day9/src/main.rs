30a83df0ad44d1aaa802f310aff541b9f9bc39fc
zemiret <dev@amleczko.pl>
2023-02-18 16:48:40 +0100

    day9 part2 - this one ain't that bad, but still not an iterator wizard

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{borrow::{BorrowMut, Borrow}, collections::HashSet};
 
 struct LineInstruction {
     dir_modif: (i32, i32),
@@ -39,6 +39,23 @@ fn debugprint(posT: (i32, i32), posH: (i32, i32)) {
     println!();
 }
 
+fn debugprint2(ropenodes: &Vec<(i32, i32)>) {
+    let mut arr = vec![vec![".".to_string(); 6]; 5];
+    arr[0][0] = "s".to_string();
+    for i in (0..ropenodes.len()).rev() {
+        let c = i.to_string();
+        arr[ropenodes[i].1 as usize][ropenodes[i].0 as usize] = c;
+    }
+    arr.reverse();
+    for row in arr {
+        for c in row {
+            print!("{} ", c);
+        }
+        println!()
+    }
+    println!();
+}
+
 fn part1() {
     let mut cur_pos_H = (0, 0);
     let mut cur_pos_T = (0, 0);
@@ -56,11 +73,10 @@ fn part1() {
                 if (cur_pos_H.0 != cur_pos_T.0
                     && cur_pos_H.1 != cur_pos_T.1
                     && ((cur_pos_H.0 - cur_pos_T.0).abs() + (cur_pos_H.1 - cur_pos_T.1).abs()) > 2)
-                    || ((cur_pos_H.0 == cur_pos_T.0
-                        || cur_pos_H.1 == cur_pos_T.1)
-                            && ((cur_pos_H.0 - cur_pos_T.0).abs()
-                                + (cur_pos_H.1 - cur_pos_T.1).abs())
-                                >= 2) // uuugllyyyyyyyy
+                    || ((cur_pos_H.0 == cur_pos_T.0 || cur_pos_H.1 == cur_pos_T.1)
+                        && ((cur_pos_H.0 - cur_pos_T.0).abs() + (cur_pos_H.1 - cur_pos_T.1).abs())
+                            >= 2)
+                // uuugllyyyyyyyy
                 {
                     cur_pos_T.0 += (cur_pos_H.0 - cur_pos_T.0).signum();
                     cur_pos_T.1 += (cur_pos_H.1 - cur_pos_T.1).signum();
@@ -69,10 +85,48 @@ fn part1() {
             }
         });
 
+    println!("{}", visited.len());
+}
+
+fn should_follow(local_head: &(i32, i32), local_tail: &(i32, i32)) -> bool {
+    (local_head.0 != local_tail.0
+        && local_head.1 != local_tail.1
+        && ((local_head.0 - local_tail.0).abs() + (local_head.1 - local_tail.1).abs()) > 2)
+        || ((local_head.0 == local_tail.0 || local_head.1 == local_tail.1)
+            && ((local_head.0 - local_tail.0).abs() + (local_head.1 - local_tail.1).abs()) >= 2)
+    // uuugllyyyyyyyy
+}
+
+fn part2() {
+    let mut ropenodes = vec![(0, 0); 10]; // 0 is head, 9 is tail
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    visited.insert((0, 0));
+
+    include_str!("../input")
+        .lines()
+        .map(parse_line)
+        .for_each(|inst| {
+            for _ in 0..inst.val {
+                ropenodes[0].0 += inst.dir_modif.0;
+                ropenodes[0].1 += inst.dir_modif.1;
+
+                for i in 0..(ropenodes.len()-1) {
+                    let local_head = ropenodes[i];
+                    let mut local_tail = ropenodes[i+1].borrow_mut();
+
+                    if should_follow(&local_head, &local_tail) {
+                        local_tail.0 += (local_head.0 - local_tail.0).signum();
+                        local_tail.1 += (local_head.1 - local_tail.1).signum();
+                    }
+                }
+
+                visited.insert(ropenodes[ropenodes.len()-1]);
+            }
+        });
 
     println!("{}", visited.len());
 }
 
 fn main() {
-    part1();
+    part2();
 }
